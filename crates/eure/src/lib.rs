9da60a4474cd3ddc04d4786ed0c960836b4d7741
Ryo Hirayama <ryo12redstone@gmail.com>
2025-12-20 23:51:32 +0900

    Fix export in eure crate

--- crates/eure/src/lib.rs
+++ crates/eure/src/lib.rs
@@ -6,4 +6,7 @@ pub mod value;
 
 pub use eure_document::Map;
 pub use eure_document::data_model;
+pub use eure_document::eure;
+pub use eure_document::parse::ParseDocument;
+pub use eure_macros::ParseDocument;
 pub use eure_parol as parol;
