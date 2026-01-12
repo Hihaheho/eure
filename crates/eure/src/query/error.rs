ddb175d4ab2143bb78026fc613752a4349dd7329
Ryo Hirayama <ryo12redstone@gmail.com>
2026-01-12 14:41:31 +0900

    Remove unused FileNotFound variant

--- crates/eure/src/query/error.rs
+++ crates/eure/src/query/error.rs
@@ -5,8 +5,6 @@ use crate::query::TextFile;
 
 #[derive(Debug, Clone, thiserror::Error)]
 pub enum EureQueryError {
-    #[error("File not found: {0}")]
-    FileNotFound(TextFile),
     #[error("Content not found: {0}")]
     ContentNotFound(TextFile),
     #[error("Parse error: {0}")]
